13c5a79b4f8d29b282439fc1fdd7666cec99a8f9
Yuichi Saito <munisystem@gmail.com>
2017-03-08 02:46:30 +0900

    Support translate from japanese to english

--- scripts/translate.js
+++ scripts/translate.js
@@ -7,12 +7,12 @@ const cheerio = require('cheerio');
 const URL = 'http://ejje.weblio.jp/content/'
 
 module.exports = robot => {
-  robot.hear(/^ja (.+)$/, msg => {
-    const en = msg.match[1];
-    got(URL+en).then(res => {
+  robot.hear(/^trans (.+)$/, msg => {
+    const str = encodeURIComponent(msg.match[1]);
+    got(URL+str).then(res => {
       const $ = cheerio.load(res.body);
       const exp = $('td.content-explanation').text();
-      msg.send(`${exp}\n>${URL+en}`)
+      msg.send(`${exp}\n>${URL+str}`)
     });
   });
 };
