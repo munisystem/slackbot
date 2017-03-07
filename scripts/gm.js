4bcb18b73e012771af7d4acf80c5ef0a0fce2d4f
Yuichi Saito <munisystem@gmail.com>
2017-03-08 03:37:16 +0900

    Tweak

--- scripts/gm.js
+++ scripts/gm.js
@@ -3,7 +3,8 @@ const cron = require('node-cron');
 
 module.exports = robot => {
   // cron.schedule('00 00 08 * * *', () => {
-  cron.schedule('00 * * * * *', () => {
+  cron.schedule('* * * * *', () => {
+    console.log('hello');
     robot.send({
       room: '#random'
     }, "@here おはよう、もう起きる時間だよ〜");
