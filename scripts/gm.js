7d537daeab8680fee2519c87bc4d36251d26d0aa
Yuichi Saito <munisystem@gmail.com>
2017-03-12 12:11:16 +0900

    Fix not responding to own message

--- scripts/gm.js
+++ scripts/gm.js
@@ -1,11 +1,15 @@
 'use strict';
+
 const cron = require('node-cron');
+
+const weather = require('../lib/weather').default;
 
 module.exports = robot => {
   cron.schedule('0 8 * * *', () => {
-    console.log('hello');
-    robot.send({
-      room: '#random'
-    }, "@here おはよう、もう起きる時間だよ〜");
+    weather().then(mes => {
+      robot.send({
+        room: '#random'
+      }, `@here おはよう、もう起きる時間だよ〜\n${mes}`);
+    });
   });
 };
